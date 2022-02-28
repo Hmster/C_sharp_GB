139bf6a1b2ef24181965cb0e1c9ed8abec44f1e2
Aleksandr Stepanov <alex_stepanoff@inbox.ru>
2022-02-28 20:35:50 +0300

    Добавьте файлы проекта.

---
+++ Lesson1_Homework/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+
+            string name = Console.ReadLine(); //объявляем переменную типа string и присваиваем ей значение, считывая данные пользователя.
+            DateTime date = DateTime.Now; //объявляем переменную типа DateTime и присваиваем ей текущее время и дату.
+
+            Console.WriteLine($"Hello, {name}. Today is {date.ToShortDateString()}"); //выводим на консоль имя и дату в заданном формате.
+            Console.ReadKey();
+        }
+    }
+}
